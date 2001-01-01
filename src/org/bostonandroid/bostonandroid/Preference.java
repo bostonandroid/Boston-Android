d641965d48a28ca04a27caa74f2380410fd7427b
danny gagne <danny@danny-gagnes-MacBook-Pro-3.local>
2000-12-31 20:15:20 -0500

    twitter stuff works, very messy needs to be cleaned up :\

---
+++ src/org/bostonandroid/bostonandroid/Preference.java
@@ -0,0 +1,18 @@
+package org.bostonandroid.bostonandroid;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class Preference extends PreferenceActivity   {
+  
+	@Override 
+	public void onCreate (Bundle savedInstanceState)
+	{	
+		super.onCreate(savedInstanceState);
+		
+		
+		
+	//	addPreferencesFromResource (R.xml.preferences_a);		
+	}
+}
--- src/org/bostonandroid/bostonandroid/Rsvp.java
+++ src/org/bostonandroid/bostonandroid/Rsvp.java
@@ -1,15 +1,26 @@
 package org.bostonandroid.bostonandroid;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Calendar;
+
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.http.AccessToken;
 import twitter4j.http.RequestToken;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,6 +29,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Rsvp extends Activity implements OnClickListener {
+
+  protected static final String TAG = "RSVP";
+  protected RequestToken rToken;
+  
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -31,46 +46,116 @@ public class Rsvp extends Activity implements OnClickListener {
   }
   
   public void onClick(View v) {
-    Intent i = new Intent(Intent.ACTION_VIEW, );
-    startActivity(i);
+	   
+    Intent i;
+	try {
+		Twitter t = new TwitterFactory().getInstance();
+	
+		Log.i(TAG, "onClick: "+ oauthToken());
+		if (oauthToken() == null)
+		{
+			Log.i(TAG, "generating request token etc");
+			
+			 t.setOAuthConsumer ("yQfelP2qk5v5UYZhLm5HQ", "q5vvqwUYWfJVVkcv7VAnSRenLRznkswSuSrl4N6Qt8");
+			 String callbackURL = "boston-android:///";
+			 rToken = t.getOAuthRequestToken(callbackURL);
+			 
+			
+			 persistRequest ();
+			 i = new Intent(Intent.ACTION_VIEW, Uri.parse(rToken.getAuthenticationURL()));
+			 startActivity(i);
+		}
+		else
+		{
+			Log.i (TAG, "doing authenticaiton");
+			Twitter tt = new TwitterFactory ().getInstance();
+			tt.setOAuthConsumer ("yQfelP2qk5v5UYZhLm5HQ", "q5vvqwUYWfJVVkcv7VAnSRenLRznkswSuSrl4N6Qt8");
+			Log.i(TAG, "OATH VERIFIER: " +oauthVerifier());
+		
+			AccessToken at = tt.getOAuthAccessToken(rToken, oauthVerifier());
+			
+			tt.updateStatus("DEV: " +Calendar.getInstance().getTime());
+			Log.i(TAG,"Updating Status Complete");
+			
+			String token = at.getToken();
+			String secret = at.getTokenSecret();
+
+			
+			Twitter t3 = new TwitterFactory ().getOAuthAuthorizedInstance("yQfelP2qk5v5UYZhLm5HQ", "q5vvqwUYWfJVVkcv7VAnSRenLRznkswSuSrl4N6Qt8", new AccessToken (token, secret));
+			t3.updateStatus("2DEV2: " +Calendar.getInstance().getTime());
+			Log.i(TAG,"DONE");
+		}
+	
+
+		
+	} catch (TwitterException e) {
+		Toast.makeText (this.getApplicationContext(), "Error: " +e, Toast.LENGTH_LONG);
+		Log.e(TAG, e.toString());
+	}
   }
+  
+  @Override
+  public void onPause ()
+  {
+	  super.onPause();
+	  
+  }
+  
+  protected void loadProvider () {
+	  try {
+	  FileInputStream fin = this.openFileInput("provider.dat");
+	  ObjectInputStream ois = new ObjectInputStream (fin);
+	  this.rToken = (RequestToken) ois.readObject();
+	  ois.close();
+	  }catch (Exception e){
+		  Log.e(TAG, e.toString());
+	  }  
+  }
+  
+  
+  
+  protected void persistRequest ()
+  {	  
+	  Log.i(TAG, "PRESISTING ");
+	  try {
+	  FileOutputStream fout = this.openFileOutput("provider.dat", MODE_PRIVATE);
+	  ObjectOutputStream oos = new ObjectOutputStream (fout);
+	  oos.writeObject (this.rToken);
+	  oos.close();
+	  }
+	  catch (Exception e) {
+		  Log.e(TAG, e.toString());
+	  }
+  }
+  
+  
   
   @Override
   public void onResume() {
     super.onResume();
-    Uri uri = getIntent().getData();
-    if (uri != null) {
-      String oauthToken = uri.getQueryParameter("oauth_token");
-      Log.e("rsvp", "oauthToken: " + oauthToken);
+    try {
+	    Uri uri = getIntent().getData();
+	    if (uri != null) {
+	      String oauthToken = uri.getQueryParameter("oauth_token");
+	      String oauthVerifier = uri.getQueryParameter("oauth_verifier");
+	      oauthToken (oauthToken);
+	      oauthVerifier (oauthVerifier);
+	      Log.i(TAG, "oauthToken: " + oauthToken);
+	      Log.i(TAG, "oauthVerifier" +oauthVerifier);
+	    }
+	    
+	    loadProvider();
+    }
+    catch (Exception e)
+    {
+    	Log.e (TAG, "Error: " +e);
     }
   }
 
+  
   private String rsvpMessage() {
     EditText text = (EditText)findViewById(R.id.rsvp_text);
     return text.getText().toString();
-  }
-  
-  private boolean isConnectedToTwitter() {
-    if (isTwitterInfoEntered()) {
-      try {
-        twitter().test();
-        return true;
-      } catch (TwitterException e) {
-        toast("Unable to connect to Twitter: " + e.getMessage());
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-  
-  private void rsvpViaTwitter(String message) {
-    try {
-      twitter().updateStatus(message);
-      toast("See you there!");
-    } catch(TwitterException e) {
-      toast("Failed to RSVP: " + e.getMessage());
-    }
   }
   
   private void startTwitterAuthPrefActivity() {
@@ -87,18 +172,6 @@ public class Rsvp extends Activity implements OnClickListener {
     return (twitterUsername() != null) && (twitterPassword() != null);
   }
   
-  private Twitter twitter() {
-    Twitter t = new TwitterFactory().getOAuthAuthorizedInstance("hETTbD1lnhQkq2MUiSBMA", "DYVzSOTv9NfICR9SAUF0wFdY2PlaM5bkqz1jRN2Xdzg");
-    try {
-      RequestToken rt = t.getOAuthRequestToken("bostonandroid://twitter");
-      t.getOAuthAccessToken(rt);
-      return t;
-    } catch (TwitterException e) {
-      Log.e("Rsvp", e.toString());
-      return null;
-    }
-  }
-  
   private String twitterUsername() {
     return twitterPreferences().getString("username", null);
   }
@@ -107,6 +180,27 @@ public class Rsvp extends Activity implements OnClickListener {
     return twitterPreferences().getString("password", null);
   }
 
+  private String  oauthVerifier ()
+  {
+	  return twitterPreferences().getString("oauthVerifier", null);
+  }
+  
+  private void oauthVerifier(String value) {
+	  Editor editor = twitterPreferences().edit(); 
+	  editor.putString("oauthVerifier", value);
+	  editor.commit();
+  }
+  
+  private String oauthToken() {
+	   return twitterPreferences().getString("oauthToken", null);
+  }
+  
+  private void oauthToken(String value) {
+	  Editor editor = twitterPreferences().edit(); 
+	  editor.putString("oauthToken", value);
+	  editor.commit();
+  }
+  
   SharedPreferences twitterPreferences() {
     return getSharedPreferences("twitter", MODE_PRIVATE);
   }
@@ -117,5 +211,32 @@ public class Rsvp extends Activity implements OnClickListener {
 
   void setWhen(String when) {
     when().setText(when);
+  }
+  
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+  	super.onCreateOptionsMenu(menu);
+  	MenuInflater inflater = getMenuInflater ();
+  	inflater.inflate (R.menu.main_menu, menu);	
+  	return true;
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected (MenuItem item) 
+  {
+  	switch (item.getItemId())
+  	{
+	    	case R.id.menu_preferences:
+	    		Log.i(TAG, "Launching Preferences");	   
+	    		startActivity (new Intent (this, Preference.class));    
+	    		break;
+  	    			
+  		default:
+  			Toast.makeText(this, "ERROR - MENU OPTION NOT SELECTED", Toast.LENGTH_SHORT).show();
+  			return true;
+  	}
+  	return true;
   }
 }
