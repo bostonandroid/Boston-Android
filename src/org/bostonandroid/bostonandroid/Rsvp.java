ddba1996d2b26250d83ccf03cc6ed10ce0f3b3cd
Boston Android <droids@lists.bostonandroid.org>
2010-10-18 20:29:13 -0400

    Initial commit

---
+++ src/org/bostonandroid/bostonandroid/Rsvp.java
@@ -0,0 +1,23 @@
+package org.bostonandroid.bostonandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Rsvp extends Activity implements OnClickListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button rsvpButton = (Button)findViewById(R.id.rsvp_button);
+        rsvpButton.setOnClickListener(this);
+    }
+    
+    public void onClick(View v) {
+      Toast.makeText(this, "You're so not RSVPed yet.", Toast.LENGTH_LONG).show();
+    }
+}
