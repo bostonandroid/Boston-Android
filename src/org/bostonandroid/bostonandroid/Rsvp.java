7e2a1744b2a01b8f779f53660d299f3035d07fe4
Mike Burns <mburns@thoughtbot.com>
2010-10-23 19:26:43 -0400

    Don't abstract pre-maturely. It doesn't work yet and still needs work before it can be abstracted.

--- src/org/bostonandroid/bostonandroid/Rsvp.java
+++ src/org/bostonandroid/bostonandroid/Rsvp.java
@@ -1,5 +1,8 @@
 package org.bostonandroid.bostonandroid;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -7,19 +10,83 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Rsvp extends Activity {
+public class Rsvp extends Activity implements OnClickListener {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    Button rsvpButton = (Button) findViewById(R.id.rsvp_button);
-    rsvpButton.setOnClickListener(new RsvpHandler(this));
-
     //alarmManager().set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 1, makePendingIntent(makeIntent()));
     sendBroadcast(new Intent(this, AlarmReceiver.class));
+    Button rsvpButton = (Button)findViewById(R.id.rsvp_button);
+    rsvpButton.setOnClickListener(this);
+  }
+  
+  public void onClick(View v) {
+    if (isConnectedToTwitter())
+      rsvpViaTwitter(rsvpMessage());
+    else
+      startTwitterAuthPrefActivity();
+  }
+  
+  private String rsvpMessage() {
+    EditText text = (EditText)findViewById(R.id.rsvp_text);
+    return text.getText().toString();
+  }
+  
+  private boolean isConnectedToTwitter() {
+    if (isTwitterInfoEntered()) {
+      try {
+        twitter().test();
+        return true;
+      } catch (TwitterException e) {
+        toast("Unable to connect to Twitter: " + e.getMessage());
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  
+  private void rsvpViaTwitter(String message) {
+    try {
+      twitter().updateStatus(message);
+      toast("See you there!");
+    } catch(TwitterException e) {
+      toast("Failed to RSVP: " + e.getMessage());
+    }
+  }
+  
+  private void startTwitterAuthPrefActivity() {
+    Intent i = new Intent(this, TwitterAuthPref.class);
+    i.putExtra("message", rsvpMessage());
+    startActivity(i);
+  }
+
+  private void toast(String s) {
+    Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+  }
+  
+  private boolean isTwitterInfoEntered() {
+    return (twitterUsername() != null) && (twitterPassword() != null);
+  }
+  
+  private Twitter twitter() {
+    return new TwitterFactory().getInstance(twitterUsername(), twitterPassword());
+  }
+  
+  private String twitterUsername() {
+    return twitterPreferences().getString("username", null);
+  }
+  
+  private String twitterPassword() {
+    return twitterPreferences().getString("password", null);
   }
 
   SharedPreferences twitterPreferences() {
---
+++ src/org/bostonandroid/bostonandroid/TwitterPoster.java
@@ -0,0 +1,10 @@
+package org.bostonandroid.bostonandroid;
+
+import android.os.AsyncTask;
+
+class TwitterPoster extends AsyncTask<Object, Object, Boolean> {  
+  @Override
+  public Boolean doInBackground(Object... o) {
+    return new Boolean(true);
+  }
+}
